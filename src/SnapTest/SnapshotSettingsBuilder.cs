4b4e295f825ad2a9d0f78c9defe705a21565b948
Chris Grinton <cgrinton@gmail.com>
2020-10-18 03:33:52 +1100

    Move generic SnapshotSettingsBuilder logic from SnapTest.NUnit to SnapTest class library so it can be used with different test framework class libraries

---
+++ src/SnapTest/SnapshotSettingsBuilder.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SnapTest
+{
+    /// <summary>
+    /// The <c>SnapshotSettingsBuilder</c> class is used to construct and initialize <see cref="SnapTest.SnapshotSettings"/> objects.
+    /// <see cref="WithSettings"/> can be called on instances of this class to
+    /// register actions which will be called immediately after a new <see cref="SnapshotSettings"/> object is created in order
+    /// to set property values in the <see cref="SnapshotSettings"/> object.
+    /// </summary>
+    public class SnapshotSettingsBuilder<SnapshotSettingsT> where SnapshotSettingsT : SnapshotSettings
+    {
+        public delegate SnapshotSettingsT SettingsFactory(IEnumerable<Action<SnapshotSettingsT>> settingsInitializers);
+
+        private SettingsFactory settingsFactory;
+        private List<Action<SnapshotSettingsT>> settingsInitializers = new List<Action<SnapshotSettingsT>>();
+
+        /// <summary>
+        /// TODO: Document
+        /// </summary>
+        /// <param name="settingsFactory"></param>
+        public SnapshotSettingsBuilder(SettingsFactory settingsFactory)
+        {
+            this.settingsFactory = settingsFactory;
+        }
+
+        /// <summary>
+        /// Create a new <see cref="SnapshotSettings"/> object, invoke initialization actions for it, and
+        /// apply default settings.
+        /// </summary>
+        /// <returns>
+        /// The created and initialized <see cref="SnapshotSettings"/> object.
+        /// </returns>
+        public SnapshotSettingsT Build()
+            => settingsFactory(settingsInitializers);
+
+        /// <summary>
+        /// Register an action to be called to set properties on a <see cref="SnapshotSettings"/> object when <see cref="Build"/> is called.
+        /// </summary>
+        /// <param name="settingsInitializer">
+        /// The action to be called to initialize settings when <see cref="Build"/> is called.
+        /// A <c>SnapshotSettingsT</c> object is passed as the single parameter to the action.
+        /// The action should set properties as appropriate on this object.
+        /// </param>
+        /// <returns>This <c>SnapshotSettingsBuilder</c>.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="settingsInitializer"/> is null.
+        /// </exception>
+        public SnapshotSettingsBuilder<SnapshotSettingsT> WithSettings(Action<SnapshotSettingsT> settingsInitializer)
+        {
+            if (settingsInitializer == null)
+                throw new ArgumentNullException(nameof(settingsInitializer));
+
+            settingsInitializers.Add(settingsInitializer);
+            return this;
+        }
+    }
+}
